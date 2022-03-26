d290ad883338adb96ee0374f465e7495e5b6678a
Kyle Lackinger <kbl40@georgetown.edu>
2022-03-26 11:47:55 -0500

    initial commit

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,7 @@
+// Address of the Whitelist Contract that was deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xC0CaAadA099904A848EC20Ee87363FCfB7e08e13"
+
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
