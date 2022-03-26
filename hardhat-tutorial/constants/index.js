8d543a3bf0af305973a1e92bffcb356a8bb67a71
Kyle Lackinger <kbl40@georgetown.edu>
2022-03-26 12:08:55 -0500

    updated metadata to work with Opensea

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -2,6 +2,6 @@
 const WHITELIST_CONTRACT_ADDRESS = "0xC0CaAadA099904A848EC20Ee87363FCfB7e08e13"
 
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://nft-collection-lw-3.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
